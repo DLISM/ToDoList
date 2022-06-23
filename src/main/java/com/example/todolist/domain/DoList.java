e581a9f5b51302334a0478aa12064eeffdc2d2f7
311 <ismoilov92@gmail.com>
2022-06-23 23:34:37 +0300

    Initial commit

---
+++ src/main/java/com/example/todolist/domain/DoList.java
@@ -0,0 +1,18 @@
+package com.example.todolist.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class DoList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String text;
+
+    private boolean done;
+
+}
