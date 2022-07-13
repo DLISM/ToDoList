f32a2e0a483a7a447fbdbcd039f5f578e0e9dc88
311 <ismoilov92@gmail.com>
2022-07-14 01:24:39 +0300

    init frontend vue webpack

--- src/main/java/com/example/todolist/controller/MainController.java
+++ src/main/java/com/example/todolist/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.example.todolist.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MainController {
 
+    @Value("${spring.profile.active}")
+    private String profile;
+
     @GetMapping
-    public String main(){
+    public String main(Model model){
+        model.addAttribute("isDevMode", true);
         return "index";
     }
 }
---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div v-if="true">
+    <h1>Welcome to My Tasks application!</h1>
+    <p>You can create to do list and spend effectively your time!</p>
+    <p>For start please <a href="/login">login</a></p>
+  </div>
+
+  <div v-else>
+    <login-page/>
+  </div>
+
+</template>
+
+<script>
+import LoginPage from "pages/login.vue"
+export default {
+  name: "App",
+  components:{
+    LoginPage
+  },
+  date(){
+    return{
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
